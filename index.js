6594330460ab63fbe54a3471a99f5a6ef3c0f756
David Harris <dahveed15@gmail.com>
2018-03-16 10:36:13 -0700

    pushing to github

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-//creating a new route handler
-app.get('/', (req, res) => {
-  res.send({hi: 'buddy'});
-});
+//passport, use GoogleStrategy (understand that users can authenticate themselves in our app)
+passport.use(new GoogleStrategy());
 
 //process.env.PORT is for heroku, 5000 is the local server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
